1da35f6b66a1cd7a86dfcbd10ce23c93ea53bdbd
Rebecca Tubridy <rebecca.tubridy@wpengine.com>
2022-04-04 12:25:17 +0100

    Added staty;e changes to article card and user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @article
+      redirect_to @user
     else
       render :edit, status: :unprocessable_entity
     end
